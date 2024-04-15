f7e850c61f90136ffbbe3c81d0da2228a7f34e55
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-04-15 15:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your code here
+() => {
+  cy.contains('3 random audios');
+  cy.get('audio').should($audios => {
+    expect($audios).to.have.length(3);
+    $audios.each((index, audio) => {
+      expect(audio).to.have.attr('controls');
+      expect(audio.src).to.be.oneOf([
+        'http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3',
+        'http://commondatastorage.googleapis.com/codeskulptor-assets/Epoq-Lepidoptera.ogg',
+        'http://codeskulptor-demos.commondatastorage.googleapis.com/pang/paza-moduless.mp3'
+      ]);
+    });
+  });
+}
+
